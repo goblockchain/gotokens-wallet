175177064e7d465dfe8aa3dcad8ee4050a6bce3d
wallace azevedo <74993539+wallaceAzevedo@users.noreply.github.com>
2022-02-03 09:17:25 -0300

    up date path contract

--- web_app/src/contracts/GoBlockchain.js
+++ web_app/src/contracts/GoBlockchain.js
@@ -1,4 +1,4 @@
 const contract = require("@truffle/contract");
 
 // export default contract(require('../gotokens-wallet-nft/build/contracts/GoBlockchain.json'))
-export default contract(require('../../../smart_contracts/contracts/GoBlockchain.json'))
+export default contract(require('../../../smart_contracts/build/contracts/GoBlockchain.json'))
